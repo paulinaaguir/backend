d3cd02c76e2faa8703e29c40bf40c0489b1d8ba0
paulinaaguir <paulinaaguirrestrada2013@gmail.com>
2024-04-06 10:02:44 -0500

    si

--- database/database.js
+++ database/database.js
@@ -4,6 +4,6 @@ export const database = new pg.Pool({
   user: "postgres",
   host: "localhost",
   database: "postgres",
-  password: "12345",
-  port: 5432,
+  password: "root",
+  port: 3434,
 }); 

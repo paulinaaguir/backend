cc50db276bee02d21d63bafda0cef487ee485dad
cariuss <guillentcarlos@gmail.com>
2024-05-08 17:54:58 -0500

    ayuda porfa

--- controllers/producto.controller.js
+++ controllers/producto.controller.js
@@ -1,14 +1,14 @@
 import { createProducto, getAllProductos, updateProduct, deleteProduct } from "../models/producto.js"
 
 export const registerProducto = async (req, res) => {
-    const { precio, referencia, marca,nombre,tipo,stock,fecha} = req.body
+    const { precio, referencia, marca, nombre, tipo, stock, fecha, url } = req.body
 
     const createdProduct = await createProducto({
-        precio, referencia, marca,nombre,tipo,stock,fecha
+        precio, referencia, marca, nombre, tipo, stock, fecha, url
     })
 
     if (!createdProduct) return res.status(400).json({ "message": "error al registrar producto" })
-    return res.status(200).json({"message": "OK"})
+    return res.status(200).json({ "message": "OK" })
 }
 
 export const getProductos = async (req, res) => {
@@ -19,24 +19,24 @@ export const getProductos = async (req, res) => {
 }
 
 export const updateProducto = async (req, res) => {
-    const { precio, referencia, marca,nombre,tipo,stock,fecha} = req.body
+    const { precio, referencia, marca, nombre, tipo, stock, fecha } = req.body
 
     const updatedProduct = await updateProduct({
-        precio, referencia, marca,nombre,tipo,stock,fecha
+        precio, referencia, marca, nombre, tipo, stock, fecha
     })
 
     if (!updatedProduct) return res.status(400).json({ "message": "error al actualizar el producto" })
-    return res.status(200).json({"message": "OK"})
+    return res.status(200).json({ "message": "OK" })
 }
 
 export const deleteProductos = async (req, res) => {
     console.log(req.body)
-    const {referencia} = req.body
+    const { referencia } = req.body
 
     const deleteProducto = await deleteProduct({
-         referencia
+        referencia
     })
 
     if (!deleteProducto) return res.status(400).json({ "message": "error al borrar producto" })
-    return res.status(200).json({"message": "OK"})
+    return res.status(200).json({ "message": "OK" })
 }

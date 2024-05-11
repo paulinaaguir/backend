b6a42cde47548a434ce8a2bf0297a475b3ff3d68
sebasrpg1232 <sebasrpg1232@gmail.com>
2024-05-11 02:39:52 -0500

    Idk

--- controllers/pdf.controller.js
+++ controllers/pdf.controller.js
@@ -7,7 +7,13 @@ export const GenerarPdf = (req, dataCallBack, endCallBack) => {
   // Pipe its output somewhere, like to a file or HTTP response
   doc.on('data', dataCallBack)
   doc.on('end', endCallBack)
-  
+  function sumatoriaTotal(datos) {
+    let suma = 0;
+    datos.forEach(element => {
+      suma += element.precio * element.cantidad;
+    });;
+    return suma;
+  }
   function fillData(datos, startX, startY) {
     // Agregar encabezado
     doc.fontSize(24).text("Factura", { align: 'center' });
@@ -15,21 +21,23 @@ export const GenerarPdf = (req, dataCallBack, endCallBack) => {
 
     // Agregar detalles de la compra
     datos.forEach(element => {
-      doc.fontSize(14).text(`Producto: ${element.nombre}`, startX, startY);
-      doc.fontSize(14).text(`Precio: $${element.precio}`, startX, startY + 20);
-      doc.fontSize(14).text(`cantidad: ${element.cantidad}`, startX, startY + 40);
+      doc.fontSize(14).text(`${element.cantidad}`, startX -15, startY);
+      doc.fontSize(14).text(`Producto: ${element.nombre}`, startX + 40 , startY );
+      doc.fontSize(14).text("------------------: ", startX + 260 , startY );
+      doc.fontSize(14).text( "$"+element.precio, startX + 360, startY);
+     
       startY += 40; // Incrementa la posición Y para el siguiente elemento
     });
 
     // Calcular el total
-    const total = datos.reduce((acc, curr) =>parseFloat(acc) + parseFloat(curr.precio), 0);
+    const total = sumatoriaTotal(datos)
     
     // Agregar total
     doc.moveDown();
-    doc.fontSize(16).text(`Total: $${total}`, { align: 'right' });
+    doc.fontSize(16).text(`Total: $${total}`, 338, startY + 100 );
   }
 
-  fillData(req.body, 50, 100);
+  fillData(req.body, 60, 120);
 
   // Finalizar y cerrar el documento
   doc.end();
